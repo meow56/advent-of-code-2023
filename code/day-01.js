827fb19dc0b7a409e03d993a1dd43dfac7c53075
meow56 <spinattacktotheface@gmail.com>
2023-11-30 23:14:09 -0600

    Day 1-1
    And so it begins again. Next time I should make sure to debug my starter code before the event...

--- code/day-01.js
+++ code/day-01.js
@@ -1,9 +1,20 @@
 "use strict";
 
 function day01(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /.+/g;
 	let entry;
+	let sum = 0;
 	while(entry = FILE_REGEX.exec(input)) {
-
+		let NUM_REGEX = /[0-9]/g;
+		let firstIndex = NUM_REGEX.exec(entry);
+		let lastIndex = firstIndex;
+		let trueLastIndex = NUM_REGEX.exec(entry);
+		while(trueLastIndex !== null) {
+			lastIndex = trueLastIndex;
+			trueLastIndex = NUM_REGEX.exec(entry);
+		}
+		let num = +(firstIndex[0] + lastIndex[0]);
+		sum += num;
 	}
+	displayCaption(`The sum is ${sum}.`);
 }
