09692432a01f266ee38d6560f707f0b84eb0ed7c
meow56 <spinattacktotheface@gmail.com>
2023-12-01 23:29:40 -0600

    Day 2-1
    How to get destroyed by a typo: part 1. (On line 37, I put `group[2] === "blue"` instead.)

--- code/day-02.js
+++ code/day-02.js
@@ -1,9 +1,56 @@
 "use strict";
 
 function day02(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /Game ([0-9]+): (.+)/g;
 	let entry;
+	let games = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		let pulls = entry[2].split("; ");
+		pulls = pulls.map(function(elem) {
+			elem = elem.split(", ");
+			elem = elem.map(function(group) {
+				let temp = group.split(" ");
+				temp[0] = +(temp[0]);
+				return temp;
+			});
+			return elem;
+		});
+		games[entry[1]] = pulls;
 	}
+
+	let isPossible = [false];
+	let sum = 0;
+	for(let i = 1; i < games.length; i++) {
+		let game = games[i];
+		for(let pull of game) {
+			for(let group of pull) {
+				if(group[1] === "red" && group[0] > 12) {
+					console.log(`Game ${i} is impossible, we pulled ${group[0]} red.`);
+					isPossible[i] = false;
+					break;
+				}
+				if(group[1] === "green" && group[0] > 13) {
+					console.log(`Game ${i} is impossible, we pulled ${group[0]} green.`);
+					isPossible[i] = false;
+					break;
+				}
+				if(group[1] === "blue" && group[0] > 14) {
+					console.log(`Game ${i} is impossible, we pulled ${group[0]} blue.`);
+					isPossible[i] = false;
+					break;
+				}
+			}
+			if(isPossible[i] === false) {
+				break;
+			}
+		}
+		if(isPossible[i] === false) {
+
+		} else {
+			console.log(`Game ${i} is possible.`);
+			isPossible[i] = true;
+			sum += i;
+		}
+	}
+	displayCaption(`The sum of possible games is ${sum}.`);
 }
