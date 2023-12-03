24e4f9f9fb0689a09d0cfff010f009bec6882871
meow56 <spinattacktotheface@gmail.com>
2023-12-02 23:42:33 -0600

    Day 3-2
    Easy modification. Just had to make sure we don't double count a number.
    
    Also I feel somewhat angy at storing info in a 2d array instead of an array of objects. Change incoming...

--- code/day-03.js
+++ code/day-03.js
@@ -14,7 +14,7 @@ function day03(input) {
 		grid.push(entry[0].split(""));
 		let partEntry;
 		while(partEntry = PART_REGEX.exec(entry)) {
-			parts.push([grid.length - 1, partEntry.index]);
+			parts.push([grid.length - 1, partEntry.index, partEntry[0] === "*", []]);
 		}
 		let numEntry;
 		while(numEntry = NUM_REGEX.exec(entry)) {
@@ -29,9 +29,10 @@ function day03(input) {
 			if(numbers[i][0] > part[0] + 1) continue;
 			if(numbers[i][2] >= part[1] - 1 && numbers[i][2] <= part[1] + 1) {
 				numbers[i][4] = true;
-			}
-			if(numbers[i][1] >= part[1] - 1 && numbers[i][1] <= part[1] + 1) {
+				if(part[2]) part[3].push(numbers[i][3]);
+			} else if(numbers[i][1] >= part[1] - 1 && numbers[i][1] <= part[1] + 1) {
 				numbers[i][4] = true;
+				if(part[2]) part[3].push(numbers[i][3]);
 			}
 		}
 	}
@@ -39,5 +40,9 @@ function day03(input) {
 	let partSum = numbers.reduce(function(acc, val) {
 		return val[4] ? acc + val[3] : acc;
 	}, 0);
+	let gearRatioSum = parts.reduce(function(acc, val) {
+		return val[2] && val[3].length === 2 ? acc + val[3][0] * val[3][1] : acc;
+	}, 0);
 	displayCaption(`The sum is ${partSum}.`);
+	displayCaption(`The gear ratio sum is ${gearRatioSum}.`);
 }
