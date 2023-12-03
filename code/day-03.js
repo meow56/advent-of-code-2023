fb7968bb1e933052f0de7261c327111e15ea6f50
meow56 <spinattacktotheface@gmail.com>
2023-12-03 00:39:56 -0600

    Day 3-3
    Classes implemented as planned. Also I added a random function so I could give each part a random color.

--- code/day-03.js
+++ code/day-03.js
@@ -1,7 +1,22 @@
 "use strict";
 
 function day03(input) {
-	
+	function MaybePartNumber(y, xStart, xEnd, value) {
+		this.y = y;
+		this.xStart = xStart;
+		this.xEnd = xEnd;
+		this.value = value;
+		this.isPartNumber = false;
+		this.adjacentPart;
+	}
+
+	function Part(y, x, isMaybeGear) {
+		this.y = y;
+		this.x = x;
+		this.isMaybeGear = isMaybeGear;
+		this.adjacentLabels = [];
+		this.color = [randInt(128, 255), randInt(128, 255), randInt(128, 255)];
+	}
 
 	const FILE_REGEX = /.+/g;
 	let grid = [];
@@ -14,35 +29,80 @@ function day03(input) {
 		grid.push(entry[0].split(""));
 		let partEntry;
 		while(partEntry = PART_REGEX.exec(entry)) {
-			parts.push([grid.length - 1, partEntry.index, partEntry[0] === "*", []]);
+			parts.push(new Part(grid.length - 1, 
+				partEntry.index, 
+				partEntry[0] === "*"));
 		}
 		let numEntry;
 		while(numEntry = NUM_REGEX.exec(entry)) {
-			let temp = [grid.length - 1, numEntry.index, numEntry.index + numEntry[0].length - 1, +(numEntry[0]), false];
+			let temp = new MaybePartNumber(grid.length - 1, 
+				numEntry.index, 
+				numEntry.index + numEntry[0].length - 1, 
+				+(numEntry[0]));
 			numbers.push(temp);
 		}
 	}
 
 	for(let part of parts) {
-		for(let i = 0; i < numbers.length; i++) {
-			if(numbers[i][0] < part[0] - 1) continue;
-			if(numbers[i][0] > part[0] + 1) continue;
-			if(numbers[i][2] >= part[1] - 1 && numbers[i][2] <= part[1] + 1) {
-				numbers[i][4] = true;
-				if(part[2]) part[3].push(numbers[i][3]);
-			} else if(numbers[i][1] >= part[1] - 1 && numbers[i][1] <= part[1] + 1) {
-				numbers[i][4] = true;
-				if(part[2]) part[3].push(numbers[i][3]);
+		for(let number of numbers) {
+			if(number.y < part.y - 1) continue;
+			if(number.y > part.y + 1) continue;
+			if(number.xStart >= part.x - 1 && number.xStart <= part.x + 1) {
+				number.isPartNumber = true;
+				number.adjacentPart = part;
+				if(part.isMaybeGear) part.adjacentLabels.push(number);
+			} else if(number.xEnd >= part.x - 1 && number.xEnd <= part.x + 1) {
+				number.isPartNumber = true;
+				number.adjacentPart = part;
+				if(part.isMaybeGear) part.adjacentLabels.push(number);
 			}
 		}
 	}
 
 	let partSum = numbers.reduce(function(acc, val) {
-		return val[4] ? acc + val[3] : acc;
+		return val.isPartNumber ? acc + val.value : acc;
 	}, 0);
 	let gearRatioSum = parts.reduce(function(acc, val) {
-		return val[2] && val[3].length === 2 ? acc + val[3][0] * val[3][1] : acc;
+		return val.isMaybeGear && val.adjacentLabels.length === 2 ? 
+			acc + val.adjacentLabels[0].value * val.adjacentLabels[1].value 
+			: acc;
 	}, 0);
 	displayCaption(`The sum is ${partSum}.`);
 	displayCaption(`The gear ratio sum is ${gearRatioSum}.`);
+	displayCaption(`The blueprint is shown.`);
+	displayCaption(`All gears and their gear ratio are shown with a black background.`);
+	displayCaption(`All non-gear parts and their part number are shown with a non-black, non-white background.`);
+	displayCaption(`Numbers that are not associated with a part are shown with a white background.`);
+
+	for(let part of parts) {
+		if(part.isMaybeGear && part.adjacentLabels.length === 2) {
+			part.color = "black";
+		}
+	}
+	for(let i = 0; i < grid.length; i++) {
+		let initialLine = new Array(grid[i].length).fill(" ");
+		for(let part of parts) {
+			if(part.y !== i) continue;
+			if(part.color === "black") {
+				initialLine[part.x] = `<span class="black">${grid[i][part.x]}</span>`;
+			} else {
+				initialLine[part.x] = `<span style="background-color: rgb(${part.color.join(", ")});">${grid[i][part.x]}</span>`;
+			}
+		}
+		for(let number of numbers) {
+			if(number.y !== i) continue;
+			if(!number.isPartNumber) {
+				let dummy = new Array(number.value.toString().length - 1).fill("");
+				initialLine.splice(number.xStart, number.value.toString().length, number.value, ...dummy);
+			} else {
+				let dummy = new Array(number.value.toString().length - 1).fill("");
+				if(number.adjacentPart.color === "black") {
+					initialLine.splice(number.xStart, number.value.toString().length, `<span class="black">${number.value}</span>`, ...dummy);
+				} else {
+					initialLine.splice(number.xStart, number.value.toString().length, `<span style="background-color: rgb(${number.adjacentPart.color.join(", ")});">${number.value}</span>`, ...dummy);
+				}
+			}
+		}
+		displayText(initialLine.join(""));
+	}
 }
