0f32fd3335c5e91bbe8c2b1518f6da575635e52b
meow56 <spinattacktotheface@gmail.com>
2023-12-03 23:18:09 -0600

    Day 4-2
    Ok, that wasn't so bad. Nice and simple multiplicity indicator solved everything.

--- code/day-04.js
+++ code/day-04.js
@@ -3,7 +3,7 @@
 function day04(input) {
 	const FILE_REGEX = /Card +(\d+): (.+) \| (.+)/g;
 	let entry;
-	let cards = [];
+	let cards = [[0, 0, 0]];
 	while(entry = FILE_REGEX.exec(input)) {
 		let winning = entry[2].split(" ");
 		winning = winning.filter(e => e !== "");
@@ -11,19 +11,29 @@ function day04(input) {
 		let nums = entry[3].split(" ");
 		nums = nums.filter(e => e !== "");
 		nums = nums.map(e => +e);
-		cards[entry[1]] = [winning, nums];
+		cards[entry[1]] = [winning, nums, 1];
 	}
 
 	let sum = 0;
 	for(let i = 1; i < cards.length; i++) {
-		let winners = 1/2;
+		let winners = -1;
 		for(let num of cards[i][1]) {
 			if(cards[i][0].includes(num)) {
-				winners *= 2;
+				winners++;
 			}
 		}
-		winners = Math.floor(winners);
-		sum += winners;
+		for(let j = 1; j <= 10; j++) {
+			if(winners >= j - 1) {
+				if(i + j < cards.length) {
+					cards[i + j][2] += cards[i][2];
+				}
+			}
+		}
+		sum += Math.floor(2 ** winners);
 	}
+	let scratchCount = cards.reduce(function(acc, val) {
+		return acc + val[2];
+	}, 0);
 	displayCaption(`The sum is ${sum}.`);
+	displayCaption(`The number of scratchcards is ${scratchCount}.`);
 }
