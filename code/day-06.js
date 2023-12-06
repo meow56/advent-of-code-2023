e946d4ea5bcb180942e49da9a3884e589e62bf04
meow56 <spinattacktotheface@gmail.com>
2023-12-05 23:11:40 -0600

    Day 6-2
    Uh oh, the input is really short. This may be a bad sign...

--- code/day-06.js
+++ code/day-06.js
@@ -1,9 +1,26 @@
 "use strict";
 
 function day06(input) {
-	const FILE_REGEX = /./g;
+	const TIME_REGEX = /Time: +(.+)/g;
+	const DIST_REGEX = /Distance: +(.+)/g;
 	let entry;
-	while(entry = FILE_REGEX.exec(input)) {
-
+	let times;
+	let distances;
+	entry = TIME_REGEX.exec(input)
+	times = entry[1].split(" ").filter(e => e !== "").map(e => +e);
+	entry = DIST_REGEX.exec(input)
+	distances = entry[1].split(" ").filter(e => e !== "").map(e => +e);
+	function calcPossible(time, distance) {
+		for(let i = 0; i < time; i++) {
+			if(i * (time - i) > distance) {
+				return time - i - i + 1;
+			}
+		}
 	}
+
+	let product = 1;
+	for(let i = 0; i < times.length; i++) {
+		product *= calcPossible(times[i], distances[i]);
+	}
+	displayCaption(`The product is ${product}.`);
 }
