00bba5700df877f65de7313512cab7ca778e573c
meow56 <spinattacktotheface@gmail.com>
2023-12-07 00:11:49 -0600

    Day 7-3
    Visualization done. Simple enough.

--- code/day-07.js
+++ code/day-07.js
@@ -38,8 +38,8 @@ function day07(input) {
 	while(entry = FILE_REGEX.exec(input)) {
 		let cards = entry[1].split("").map(e => CARD_VAL.get(e));
 		let jokeCards = entry[1].split("").map(e => JOKE_CARD_VAL.get(e));
-		handsAndBids.push([cards, +entry[2], getType(cards)]);
-		jokeHands.push([jokeCards, +entry[2], getType(jokeCards, true)]);
+		handsAndBids.push([cards, +entry[2], getType(cards), entry[1].split("")]);
+		jokeHands.push([jokeCards, +entry[2], getType(jokeCards, true), entry[1].split("")]);
 	}
 
 	function getType(cards, part2 = false) {
@@ -92,5 +92,44 @@ function day07(input) {
 		return acc + (val[1] * (ind + 1));
 	}, 0);
 	displayCaption(`The winnings are ${winnings}.`);
-	displayCaption(`The 2nd round of winnings are ${jokeWinnings}.`);
+	displayCaption(`The second round of winnings are ${jokeWinnings}.`);
+	displayCaption(`The ranking of all hands is displayed.`);
+	displayCaption(`The left half is for part 1, the right for part 2.`);
+	displayCaption(`The ranks start at 1 and go to 1000. The type of the hand is also displayed.`);
+	displayCaption(`The winnings are also shown for every hand.`);
+
+	const maxRankLength = (handsAndBids.length + 1).toString().length;
+	const maxBidLength = handsAndBids.reduce((acc, val) => Math.max(acc, val[1].toString().length), 0);
+	const maxWinLength = handsAndBids.reduce((acc, val, ind) => Math.max(acc, (val[1] * (ind + 1)).toString().length), 0);
+	const maxJinLength = jokeHands.reduce((acc, val, ind) => Math.max(acc, (val[1] * (ind + 1)).toString().length), 0);
+	
+	const HAND_DISPLAY = new Map([
+		[6, "5 of a Kind"],
+		[5, "4 of a Kind"],
+		[4, " Full House"],
+		[3, "3 of a Kind"],
+		[2, "  Two Pair "],
+		[1, "  One Pair "],
+		[0, " High Card "],
+		]);
+	for(let i = 0; i < handsAndBids.length; i++) {
+		let rank = (i + 1).toString().padStart(maxRankLength, " ");
+		let finalText = `Rank ${rank}:   `;
+		let hand = handsAndBids[i][3].join(" ");
+		finalText += hand;
+		let handType = HAND_DISPLAY.get(handsAndBids[i][2]);
+		let bid = handsAndBids[i][1].toString().padStart(maxBidLength, " ");
+		let product = (handsAndBids[i][1] * (i + 1)).toString().padStart(maxWinLength, " ");
+		finalText += `   (${handType}) => ${bid} * ${rank} = ${product}`;
+
+		finalText += ` | `
+		finalText += `Rank ${rank}:   `;
+		let jokeHand = jokeHands[i][3].join(" ");
+		finalText += jokeHand;
+		let jokeHandType = HAND_DISPLAY.get(jokeHands[i][2]);
+		let jokeBid = jokeHands[i][1].toString().padStart(maxBidLength, " ");
+		let jokeProduct = (jokeHands[i][1] * (i + 1)).toString().padStart(maxJinLength, " ");
+		finalText += `   (${jokeHandType}) => ${jokeBid} * ${rank} = ${jokeProduct}`;
+		displayText(finalText);
+	}
 }
