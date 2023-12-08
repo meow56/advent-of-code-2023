4d48efd03cdba21756d6b5f4df53d23975c438ef
meow56 <spinattacktotheface@gmail.com>
2023-12-08 01:24:20 -0600

    Day 8-2.1
    Ok, here's my previous solution. I have checked it against 2 inputs and it has given the right answer both times, so that means it must be 100% correct the entire time.

--- code/day-08.js
+++ code/day-08.js
@@ -1,8 +1,6 @@
 "use strict";
 
 function day08(input) {
-	// 7309459565208 too high
-	// 14618919050608 too high
 	function factor(n) {
 		let factors = [];
 		for(let i = 2; i < Math.sqrt(n); i++) {
@@ -15,28 +13,6 @@ function day08(input) {
 		factors.push(n);
 		return factors.sort((a, b) => a - b);
 	}
-
-	function exEuclid(a, b) {
-		let r = [a, b];
-		let s = [1n, 0n];
-		let t = [0n, 1n];
-		while(r[r.length - 1] !== 0n) {
-			let i = r.length - 1;
-			let q = r[i - 1] / r[i];
-			r.push(r[i - 1] % r[i]);
-			s.push(s[i - 1] - (q * s[i]));
-			t.push(t[i - 1] - (q * t[i]));
-		}
-		let i = r.length - 2;
-		if(BigInt(r[i]) !== (BigInt(a) * BigInt(s[i])) + (BigInt(b) * BigInt(t[i]))) {
-			console.log(r);
-			console.log(s);
-			console.log(t);
-			throw `You implemented the algorithm wrong mate`;
-		}
-		return [r[i], s[i], t[i]];
-	}
-
 	function Node(name, left, right) {
 		this.name = name;
 		this.left = left;
@@ -101,27 +77,26 @@ function day08(input) {
 							// s.t. f[i] + n_i * cycles[i] = N for all i.
 							
 							// Time to plagiarize from my 2020 day 13 solution.
-							console.log(cycles);
-							console.log(firstEncounter.map(e => e[0]));
 							let factoredCycles = cycles.map(cycle => factor(cycle));
 							// Looking at my own input, I'm pretty sure every cycle
 							// will have a common factor. So it's assumption time.
-							// Update: CRM doesn't work out of the box with noncoprime
-							// bases. Wikipedia saves the day with an extension!
-							// We need to use Bezout's (accents) identity.
-							let num = BigInt(cycles[0]);
-							let lastDiv;
-							let lastG;
-							for(let j = 0; j < cycles.length - 1; j++) {
-								let newNum = num;
-								if(j !== 0) lastDiv = (num * BigInt(cycles[j]) / lastG);
-								//while(newNum < 0) newNum += lastDiv;
-								let [g, u, v] = exEuclid(newNum, BigInt(cycles[j + 1]));
-								lastG = g;
-								num = (BigInt(firstEncounter[j][0]) * v * BigInt(cycles[j + 1]) + BigInt(firstEncounter[j + 1][0]) * u * num) / g;
+							let commonFactor;
+							for(let testFactor of factoredCycles[0]) {
+								if(factoredCycles.every(e => e.includes(testFactor))) {
+									commonFactor = testFactor;
+									break;
+								}
 							}
-							allSteps = num % lastDiv;
-							while(allSteps < 0) allSteps += -1n * lastDiv;
+							let base = 1;
+							let num = 0;
+							for(let j = 0; j < startNodes.length; j++) {
+								while((num + firstEncounter[j][0]) % cycles[j] !== 0) {
+									num += base;
+								}
+								base *= cycles[j] / commonFactor;
+								if(j === 0) base *= commonFactor;
+							}
+							allSteps = num - 1; // Why is it - 1? Don't ask.
 							finished = true;
 						}
 					}
