9d0d65847b9a1af5a31e64ec67678ffa783d833a
meow56 <spinattacktotheface@gmail.com>
2023-12-07 23:21:18 -0600

    Day 8-1
    This one was quite annoying since I had to debug an infinite loop. What a pain.

--- code/day-08.js
+++ code/day-08.js
@@ -1,9 +1,41 @@
 "use strict";
 
 function day08(input) {
-	const FILE_REGEX = /./g;
-	let entry;
-	while(entry = FILE_REGEX.exec(input)) {
-
+	function Node(name, left, right) {
+		this.name = name;
+		this.left = left;
+		this.right = right;
 	}
+
+	Node.prototype.initialize = function() {
+		for(let i = 0; i < nodes.length; i++) {
+			let node = nodes[i];
+			if(node.name === this.left) this.left = i;
+			if(node.name === this.right) this.right = i;
+		}
+	}
+
+	const INST_REGEX = /([LR]+)\n/g;
+	const NODE_REGEX = /([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)/g;
+	let entry;
+	let instructions = INST_REGEX.exec(input)[1];
+	let nodes = [];
+	while(entry = NODE_REGEX.exec(input)) {
+		nodes.push(new Node(entry[1], entry[2], entry[3]));
+	}
+	let currNode;
+	for(let node of nodes) {
+		node.initialize();
+		if(node.name === "AAA") currNode = node;
+	}
+	let currInst = 0;
+	let steps = 0;
+	while(currNode.name !== "ZZZ") {
+		if(instructions[currInst] === "L") currNode = nodes[currNode.left];
+		else currNode = nodes[currNode.right];
+		currInst = (currInst + 1) % instructions.length;
+		steps++;
+	}
+
+	displayCaption(`The number of steps is ${steps}.`);
 }
