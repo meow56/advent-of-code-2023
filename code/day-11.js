52c55b9eda2f007f50fe843c8203d19c4f018657
meow56 <spinattacktotheface@gmail.com>
2023-12-10 23:31:37 -0600

    Day 11-2
    Just had to rewrite the solution, no biggee.

--- code/day-11.js
+++ code/day-11.js
@@ -9,16 +9,14 @@ function day11(input) {
 	}
 
 	let newGrid = [];
-	for(let row of grid) {
-		if(!row.includes("#")) {
-			newGrid.push(row.slice());
-			newGrid.push(row.slice());
-		} else {
-			newGrid.push(row.slice());
+	let emptyRows = [];
+	for(let y = 0; y < grid.length; y++) {
+		if(!grid[y].includes("#")) {
+			emptyRows.push(y);
 		}
 	}
 
-	let changes = 0;
+	let emptyColumns = [];
 	for(let x = 0; x < grid[0].length; x++) {
 		let isEmpty = true;
 		for(let y = 0; y < grid.length; y++) {
@@ -27,37 +25,51 @@ function day11(input) {
 			}
 		}
 		if(isEmpty) {
-			for(let y = 0; y < newGrid.length; y++) {
-				newGrid[y].splice(x + changes, 0, ".");
-			}
-			changes++;
+			emptyColumns.push(x);
 		}
 	}
 
-	for(let row of newGrid) {
-		displayText(row.join(""));
-	}
-
 	let galaxies = [];
-	for(let y = 0; y < newGrid.length; y++) {
-		for(let x = 0; x < newGrid[y].length; x++) {
-			if(newGrid[y][x] === "#") {
+	for(let y = 0; y < grid.length; y++) {
+		for(let x = 0; x < grid[y].length; x++) {
+			if(grid[y][x] === "#") {
 				galaxies.push([x, y]);
 			}
 		}
 	}
 
-	function manhattan(point1, point2) {
-		return Math.abs(point2[1] - point1[1]) + Math.abs(point2[0] - point1[0]);
+	function manhattan(point1, point2, part2 = false) {
+		let uncorrectedDist = Math.abs(point2[1] - point1[1]) + Math.abs(point2[0] - point1[0]);
+		let crossedX = 0;
+		for(let row of emptyRows) {
+			if(point1[1] < row && point2[1] > row) {
+				crossedX++;
+			} else if(point1[1] > row && point2[1] < row) {
+				crossedX++;
+			}
+		}
+		let crossedY = 0;
+		for(let column of emptyColumns) {
+			if(point1[0] < column && point2[0] > column) {
+				crossedY++;
+			} else if(point1[0] > column && point2[0] < column) {
+				crossedY++;
+			}
+		}
+		let correctionFactor = part2 ? (1000000 - 1) : (2 - 1);
+		return uncorrectedDist + ((crossedX + crossedY) * correctionFactor);
 	}
 
 	let sum = 0;
+	let sum2 = 0;
 	for(let i = 0; i < galaxies.length; i++) {
 		for(let j = i + 1; j < galaxies.length; j++) {
 			let dist = manhattan(galaxies[i], galaxies[j]);
-			console.log(`dist(${i + 1}, ${j + 1}): ${dist}`);
+			let dist2 = manhattan(galaxies[i], galaxies[j], true);
 			sum += dist;
+			sum2 += dist2;
 		}
 	}
 	displayCaption(`The sum is ${sum}.`);
+	displayCaption(`The sum is ${sum2}.`);
 }
