521c230ee348446b465b39b9cf8c6c6f2d4c2ee9
meow56 <spinattacktotheface@gmail.com>
2023-12-12 22:16:32 -0600

    Day 12-2
    Yay! I got to use dynamic programming, let's go! And it's actually not terribly slow, woohoo!

--- code/day-12.js
+++ code/day-12.js
@@ -1,11 +1,15 @@
 "use strict";
 
-function day12(input) {	
+function day12(input) {
 	const FILE_REGEX = /(.+) (.+)/g;
 	let rows = [];
+	let unfolded = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
 		rows.push([entry[1], entry[2].split(",").map(e => +e)]);
+		let realRow = new Array(5).fill(entry[1]).join("?");
+		let realClue = new Array(5).fill(entry[2]).join(",").split(",").map(e => +e);
+		unfolded.push([realRow, realClue]);
 	}
 
 	function count(string, char) {
@@ -42,45 +46,65 @@ function day12(input) {
 		return 0;
 	}
 
-	let sum = 0;
-	for(let row of rows) {
-		let clue = row[1];
-		let clueCount = clue.reduce((acc, val) => acc + val, 0);
-		let filter = row[0];
-		let clueLength = clue.reduce((acc, val) => acc + val + 1, -1);
-		let extraSpace = filter - clueLength;
-		//console.log(`Now doing "${filter}".`);
-		//console.log(`With clue "${clue.join(", ")}".`);
-		let questions = count(filter, "?");
-		let hexCount = count(filter, "#");
-		let possibilities = [];
-		for(let i = 0; i < 2 ** questions; i++) {
-			let binaryForm = i.toString(2).padStart(questions, "0").split("");
-			let numHex = count(binaryForm, "1");
-			if(numHex + hexCount !== clueCount) continue;
-			let newPoss = "";
-			for(let j = 0; j < filter.length; j++) {
-				if(filter[j] === "?") {
-					if(binaryForm.pop() === "1") {
-						newPoss += "#";
-					} else {
-						newPoss += ".";
-					}
-				} else {
-					newPoss += filter[j];
-				}
-			}
 
-			let newClue = generateClue(newPoss);
-			//console.log(`Possibly "${newPoss}"?`);
-			if(compare(clue, newClue) === 0) {
-				possibilities.push(newPoss);
+
+	const POSS_MAP = new Map();
+	// There are two cases.
+	// Case 1: We don't use the current character to fulfill a clue.
+	// Then getPoss(row, clues) = getPoss(row[1:], clues);
+	// Case 2: The current character is the start of a new clue.
+	// Then getPoss(row, clues) = getPoss(row[clues[0]:], clues[1:]);
+	function getPoss(row, clues) {
+		if(clues.length === 0) return +(row.split("").every(e => e !== "#"));
+		if(row.length < clues.reduce((acc, val) => acc + val, 0)) return 0;
+
+		let MAP_KEY = row + " " + clues.join();
+		if(POSS_MAP.has(MAP_KEY)) {
+			return POSS_MAP.get(MAP_KEY);
+		}
+
+		if(row[0] === ".") {
+			let temp = getPoss(row.slice(1), clues);
+			POSS_MAP.set(MAP_KEY, temp);
+			return temp;
+		}
+		if(row[0] === "#") {
+			let clueLength = clues[0];
+			for(let i = 0; i < clueLength; i++) {
+				if(row[i] === ".") return 0; // Not happening.
 			}
+			if(row[clueLength] === "#") return 0; // Not happening.
+			let temp = getPoss(row.slice(clueLength + 1), clues.slice(1));
+			POSS_MAP.set(MAP_KEY, temp);
+			return temp;
 		}
-		sum += possibilities.length;
-		for(let poss of possibilities) {
-			//console.log(poss);
+
+		let noUse = getPoss(row.slice(1), clues);
+		let clueLength = clues[0];
+		let yesUse;
+		for(let i = 0; i < clueLength; i++) {
+			if(row[i] === ".") return noUse; // Not happening.
 		}
+		if(clueLength < row.length && row[clueLength] === "#") return noUse; // Not happening.
+		yesUse = getPoss(row.slice(clueLength + 1), clues.slice(1));
+		POSS_MAP.set(MAP_KEY, noUse + yesUse);
+		return noUse + yesUse;
+	}
+
+	let sum = 0;
+	for(let i = 0; i < rows.length; i++) {
+		let split = rows[i][0].split(".").filter(e => e.length !== 0).join(".");
+		let clues = rows[i][1];
+		let temp = getPoss(split, clues);
+		sum += temp;
+	}
+	let otherSum = 0;
+	for(let i = 0; i < unfolded.length; i++) {
+		let split = unfolded[i][0].split(".").filter(e => e.length !== 0).join(".");
+		let clues = unfolded[i][1];
+		let temp = getPoss(split, clues);
+		otherSum += temp;
 	}
 	displayCaption(`The sum is ${sum}.`);
+	displayCaption(`The other sum is ${otherSum}.`);
 }
