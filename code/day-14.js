1ef8a8826d5162306acf0de1517dc4c59e4e11dc
meow56 <spinattacktotheface@gmail.com>
2023-12-14 01:15:23 -0600

    Day 14-2
    Man, I just could not get the modulo stuff at the end. Guess I spent all my energy debugging north/west/south/east.

--- code/day-14.js
+++ code/day-14.js
@@ -5,40 +5,151 @@ function day14(input) {
 	let entry;
 	let rocks = [];
 	const NORTH_MAP = new Map();
+	const SOUTH_MAP = new Map();
+	const EAST_MAP = new Map();
+	const WEST_MAP = new Map();
 	while(entry = FILE_REGEX.exec(input)) {
 		rocks.push(entry[0].split(""));
 	}
 
 	let rockPos = [];
 	for(let col = 0; col < rocks[0].length; col++) {
-		let mapArr = [];
-		let thisColRocks = [];
-		let firstStop = 0;
+		let northMapArr = [];
+		let firstStopNorth = 0;
 		for(let row = 0; row < rocks.length; row++) {
 			if(rocks[row][col] === "#") {
-				firstStop = row + 1;
+				firstStopNorth = row + 1;
 			} else {
-				if(rocks[row][col] === "O") thisColRocks.push(row);
-				mapArr[row] = firstStop;
+				if(rocks[row][col] === "O") rockPos.push([row, col]);
+				northMapArr[row] = firstStopNorth;
 			}
 		}
-		rockPos.push(thisColRocks); // But is it really that cool?
-		NORTH_MAP.set(col, mapArr);
+		let southMapArr = [];
+		let firstStopSouth = rocks.length;
+		for(let row = rocks.length - 1; row >= 0; row--) {
+			if(rocks[row][col] === "#") {
+				firstStopSouth = row - 1;
+			} else {
+				southMapArr[row] = firstStopSouth;
+			}
+		}
+		NORTH_MAP.set(col, northMapArr);
+		SOUTH_MAP.set(col, southMapArr);
+	}
+
+	for(let row = 0; row < rocks.length; row++) {
+		let eastMapArr = [];
+		let firstStopEast = rocks[row].length;
+		for(let col = rocks[row].length - 1; col >= 0; col--) {
+			if(rocks[row][col] === "#") {
+				firstStopEast = col - 1;
+			} else {
+				eastMapArr[col] = firstStopEast;
+			}
+		}
+		let westMapArr = [];
+		let firstStopWest = 0;
+		for(let col = 0; col < rocks.length; col++) {
+			if(rocks[row][col] === "#") {
+				firstStopWest = col + 1;
+			} else {
+				westMapArr[col] = firstStopWest;
+			}
+		}
+		EAST_MAP.set(row, eastMapArr);
+		WEST_MAP.set(row, westMapArr);
 	}
 
 	function north() {
 		for(let col = 0; col < rocks[0].length; col++) {
-			let thisRocks = rockPos[col];
+			let thisRocks = [];
+			for(let i = 0; i < rockPos.length; i++) {
+				if(rockPos[i][1] === col) {
+					thisRocks.push(rockPos.splice(i, 1)[0]);
+					i--;
+				}
+			}
+			if(thisRocks.length === 0) continue;
 			let colMap = NORTH_MAP.get(col);
-			let ghostRockPos = thisRocks.map(e => colMap[e]);
+			let ghostRockPos = thisRocks.map(e => [colMap[e[0]], col]);
 			let prevPos = -1;
 			for(let i = 0; i < ghostRockPos.length; i++) {
-				if(ghostRockPos[i] <= prevPos) {
-					ghostRockPos[i] = prevPos + 1;
+				if(ghostRockPos[i][0] <= prevPos) {
+					ghostRockPos[i][0] = prevPos + 1;
 				}
-				prevPos = ghostRockPos[i];
+				prevPos = ghostRockPos[i][0];
 			}
-			rockPos[col] = ghostRockPos.slice();
+			rockPos.push(...ghostRockPos);
+		}
+	}
+
+	function south() {
+		for(let col = 0; col < rocks[0].length; col++) {
+			let thisRocks = [];
+			for(let i = 0; i < rockPos.length; i++) {
+				if(rockPos[i][1] === col) {
+					thisRocks.push(rockPos.splice(i, 1)[0]);
+					i--;
+				}
+			}
+			if(thisRocks.length === 0) continue;
+			let colMap = SOUTH_MAP.get(col);
+			let ghostRockPos = thisRocks.map(e => [colMap[e[0]], col]);
+			let prevPos = rocks.length;
+			for(let i = ghostRockPos.length - 1; i >= 0; i--) {
+				if(ghostRockPos[i][0] >= prevPos) {
+					ghostRockPos[i][0] = prevPos - 1;
+				}
+				prevPos = ghostRockPos[i][0];
+			}
+			rockPos.push(...ghostRockPos);
+		}
+	}
+
+
+	function east() {
+		for(let row = 0; row < rocks.length; row++) {
+			let thisRocks = [];
+			for(let i = 0; i < rockPos.length; i++) {
+				if(rockPos[i][0] === row) {
+					thisRocks.push(rockPos.splice(i, 1)[0]);
+					i--;
+				}
+			}
+			if(thisRocks.length === 0) continue;
+			let rowMap = EAST_MAP.get(row);
+			let ghostRockPos = thisRocks.map(e => [row, rowMap[e[1]]]);
+			let prevPos = rocks[0].length;
+			for(let i = ghostRockPos.length - 1; i >= 0; i--) {
+				if(ghostRockPos[i][1] >= prevPos) {
+					ghostRockPos[i][1] = prevPos - 1;
+				}
+				prevPos = ghostRockPos[i][1];
+			}
+			rockPos.push(...ghostRockPos);
+		}
+	}
+
+	function west() {
+		for(let row = 0; row < rocks.length; row++) {
+			let thisRocks = [];
+			for(let i = 0; i < rockPos.length; i++) {
+				if(rockPos[i][0] === row) {
+					thisRocks.push(rockPos.splice(i, 1)[0]);
+					i--;
+				}
+			}
+			if(thisRocks.length === 0) continue;
+			let rowMap = WEST_MAP.get(row);
+			let ghostRockPos = thisRocks.map(e => [row, rowMap[e[1]]]);
+			let prevPos = -1;
+			for(let i = 0; i < ghostRockPos.length; i++) {
+				if(ghostRockPos[i][1] <= prevPos) {
+					ghostRockPos[i][1] = prevPos + 1;
+				}
+				prevPos = ghostRockPos[i][1];
+			}
+			rockPos.push(...ghostRockPos);
 		}
 	}
 
@@ -51,26 +162,49 @@ function day14(input) {
 		}
 
 		for(let i = 0; i < rockPos.length; i++) {
-			for(let j = 0; j < rockPos[i].length; j++) {
-				newRocks[rockPos[i][j]][i] = "O";
-			}
+			newRocks[rockPos[i][0]][rockPos[i][1]] = "O";
 		}
 		for(let i = 0; i < newRocks.length; i++) {
 			let row = newRocks[i];
-			displayText(`${i.toString().padStart(3, " ")} ${row.join("")}`);
+			displayText(`${row.join("")}`);
 		}
 	}
 
-	function count() {
-		return rockPos.reduce(function(acc, val) {
-			return acc + val.reduce(function(a, v) {
-				return a + rocks.length - v;
-			}, 0);
+	function count(arr = rockPos) {
+		return arr.reduce(function(acc, val) {
+			return acc + rocks.length - val[0];
 		}, 0);
 	}
 
-	north();
-	displayCaption(`The sum is ${count()}.`);
-	display();
+	const ROCK_MAP = new Map();
+	for(let i = 1; i <= 1000000000; i++) {
+		north();
+		if(i === 1) {
+			displayCaption(`The total load is ${count()}.`);
+		}
+		west();
+		south();
+		east();
+		let mapKey = rockPos.map(e => `${e.join(", ")}`).join(";");
+		if(ROCK_MAP.has(mapKey)) {
+			let cycleLength = i - ROCK_MAP.get(mapKey);
+			let offset = ROCK_MAP.get(mapKey);
+			console.log(`Repeats every ${cycleLength}, starting at ${offset}`);
+			
+			let billOff = (1000000000 - offset) % cycleLength + offset;
+			for(let entry of ROCK_MAP.entries()) {
+				if(entry[1] === billOff) {
+					rockPos = entry[0].split(";").map(e => e.split(", ").map(n => +n));
+					break;
+				}
+			}
 
+
+			break;
+		}
+		ROCK_MAP.set(mapKey, i);
+	}
+	displayCaption(`The final total load is ${count()}.`);
+	displayCaption(`The grid as it appears after the 1000000000th cycle is shown.`);
+	display();
 }
