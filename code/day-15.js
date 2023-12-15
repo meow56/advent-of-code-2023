3289714622648f148f722f2dcdd16b0806db4478
meow56 <spinattacktotheface@gmail.com>
2023-12-14 23:05:56 -0600

    Day 15-1
    Something's wrong, I can feel it. It's too easy...

--- code/day-15.js
+++ code/day-15.js
@@ -1,9 +1,23 @@
 "use strict";
 
 function day15(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /.+/g;
+	let words = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
-
+		words = entry[0].split(",");
 	}
+
+	function hash(str) {
+		let currVal = 0;
+		for(let i = 0; i < str.length; i++) {
+			let code = str.charCodeAt(i);
+			currVal += code;
+			currVal *= 17;
+			currVal %= 256;
+		}
+		return currVal;
+	}
+	let sum = words.map(e => hash(e)).reduce((acc, val) => acc + val);
+	displayCaption(`The sum is ${sum}.`);
 }
