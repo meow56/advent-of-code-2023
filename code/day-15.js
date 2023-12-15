99f452eb28bae748eb4ab4c0b1adbb7be63a4314
meow56 <spinattacktotheface@gmail.com>
2023-12-14 23:19:21 -0600

    Day 15-2
    It feels a bit like I'm cheating, using the built in Map...Well, whatever works.

--- code/day-15.js
+++ code/day-15.js
@@ -18,6 +18,41 @@ function day15(input) {
 		}
 		return currVal;
 	}
+
+	let boxes = [];
+	for(let i = 0; i < 256; i++) {
+		boxes.push(new Map());
+	}
+	
+	function hashmap(str) {
+		if(str.includes("-")) {
+			let toHash = str.split("-")[0];
+			let hashVal = hash(toHash);
+			let box = boxes[hashVal];
+			box.delete(toHash);
+		} else {
+			let info = str.split("=");
+			let label = info[0];
+			let focal = +(info[1]);
+			let hashVal = hash(label);
+			let box = boxes[hashVal];
+			box.set(label, focal);
+		}
+	}
 	let sum = words.map(e => hash(e)).reduce((acc, val) => acc + val);
+	for(let i = 0; i < words.length; i++) {
+		hashmap(words[i]);
+	}
+	let focusPowerSum = 0;
+	for(let i = 0; i < boxes.length; i++) {
+		let boxIndex = 1;
+		let focusPowerBox = 0;
+		for(let lens of boxes[i].values()) {
+			focusPowerBox += (i + 1) * boxIndex * lens;
+			boxIndex++;
+		}
+		focusPowerSum += focusPowerBox;
+	}
 	displayCaption(`The sum is ${sum}.`);
+	displayCaption(`The focus power is ${focusPowerSum}.`);
 }
