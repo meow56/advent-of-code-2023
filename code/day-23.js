c6071f5e38f066eee5ebc95074f74a1a5ccc46cb
meow56 <spinattacktotheface@gmail.com>
2023-12-22 23:59:11 -0600

    Day 23-2
    Commenting out part of your code for the win, everyone.

--- code/day-23.js
+++ code/day-23.js
@@ -7,15 +7,18 @@ function day23(input) {
 		this.type = type;
 
 		this.depth = Infinity;
+		this.explored = false;
+
+		this.targetDepths = [];
 	}
 
 	Node.prototype.initialize = function() {
 		if(this.type === "#") return;
-		if(grid[this.pos[0]][this.pos[1]] === ">") {
-			this.exits.push(find([this.pos[0], this.pos[1] + 1]));
-		} else if(grid[this.pos[0]][this.pos[1]] === "v") {
-			this.exits.push(find([this.pos[0] + 1, this.pos[1]]));
-		} else {
+		//if(grid[this.pos[0]][this.pos[1]] === ">") {
+		//	this.exits.push(find([this.pos[0], this.pos[1] + 1]));
+		//} else if(grid[this.pos[0]][this.pos[1]] === "v") {
+		//	this.exits.push(find([this.pos[0] + 1, this.pos[1]]));
+		//} else {
 			let upNeigh = find([this.pos[0] - 1, this.pos[1]]);
 			let leftNeigh = find([this.pos[0], this.pos[1] - 1]);
 			let downNeigh = find([this.pos[0] + 1, this.pos[1]]);
@@ -25,7 +28,16 @@ function day23(input) {
 			if(leftNeigh !== undefined && leftNeigh.type !== "#") this.exits.push(leftNeigh);
 			if(downNeigh !== undefined && downNeigh.type !== "#") this.exits.push(downNeigh);
 			if(rightNeigh !== undefined && rightNeigh.type !== "#") this.exits.push(rightNeigh);
-		}
+
+			let slopeCount = 0;
+			for(let neighbor of this.exits) {
+				if(neighbor.type !== ".") slopeCount++;
+			}
+			if(slopeCount > 1) {
+				this.type = "X";
+				targetNodes.push(this);
+			}
+		//}
 	}
 
 	function find(pos) {
@@ -50,10 +62,65 @@ function day23(input) {
 		nodes.push(nodeRow);
 	}
 
+	let targetNodes = [];
+
 	for(let row of nodes) {
 		for(let node of row) {
 			node.initialize();
 		}
+	}
+	targetNodes.push(nodes[0][1]);
+	targetNodes.push(nodes[nodes.length - 1][nodes[nodes.length - 1].length - 2]);
+
+	// Time to plagiarize from my 2023 day 21 (plagiarization)^2 of my 2023 day 10 plagiarization of my 2022 day 18 solution.
+	function BFS(...start) {
+		let queue = start;
+		for(let row of nodes) {
+			for(let node of row) {
+				node.depth = Infinity;
+				node.explored = false;
+			}
+		}
+		for(let node of start) {
+			node.depth = 0;
+			node.explored = true;
+		}
+		while(queue.length !== 0) {
+			let next = queue.shift();
+			for(let path of next.exits) {
+				if(!path.explored) {
+					path.explored = true;
+					path.depth = next.depth + 1;
+					if(path.type !== "X") queue.push(path);
+				}
+			}
+		}
+	}
+
+	for(let node of targetNodes) {
+		BFS(node);
+		for(let neighNode of targetNodes) {
+			if(neighNode !== node && neighNode.depth < 1000000) {
+				node.targetDepths.push([neighNode, neighNode.depth]);
+			}
+		}
+	}
+
+	function hike2(visited, length = 0) {
+		let currNode = visited[visited.length - 1];
+		if(currNode === targetNodes[targetNodes.length - 1]) {
+			return length;
+		}
+		let lengths = [];
+		for(let neighbor of currNode.targetDepths) {
+			if(!visited.includes(neighbor[0])) {
+				lengths.push(hike2([...visited, neighbor[0]], length + neighbor[1] ));
+			}
+		}
+		if(lengths.length === 0) {
+			return 0;
+		}
+		return Math.max(...lengths);
 	}
 
 	function hike(visited) {
@@ -67,9 +134,12 @@ function day23(input) {
 				lengths.push(hike([...visited, neighbor]));
 			}
 		}
+		if(lengths.length === 0) {
+			return 0;
+		}
 		return Math.max(...lengths);
 	}
 
-	let trail = hike([nodes[0][1]]);
-	displayCaption(`The max length is ${trail - 1}.`);
+	let trail = hike2([nodes[0][1]]);
+	displayCaption(`The max length is ${trail}.`);
 }
