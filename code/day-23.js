153ce074a06b333157e171fc83141dd79dc07ae8
meow56 <spinattacktotheface@gmail.com>
2023-12-23 00:13:53 -0600

    Day 23-3
    Ok, now both answers are shown at the same time. Just had to make a bunch of duplicate variables.

--- code/day-23.js
+++ code/day-23.js
@@ -12,17 +12,17 @@ function day23(input) {
 		this.targetDepths = [];
 	}
 
-	Node.prototype.initialize = function() {
+	Node.prototype.initialize = function(nodes, part2) {
 		if(this.type === "#") return;
-		//if(grid[this.pos[0]][this.pos[1]] === ">") {
-		//	this.exits.push(find([this.pos[0], this.pos[1] + 1]));
-		//} else if(grid[this.pos[0]][this.pos[1]] === "v") {
-		//	this.exits.push(find([this.pos[0] + 1, this.pos[1]]));
-		//} else {
-			let upNeigh = find([this.pos[0] - 1, this.pos[1]]);
-			let leftNeigh = find([this.pos[0], this.pos[1] - 1]);
-			let downNeigh = find([this.pos[0] + 1, this.pos[1]]);
-			let rightNeigh = find([this.pos[0], this.pos[1] + 1]);
+		if(!part2 && grid[this.pos[0]][this.pos[1]] === ">") {
+			this.exits.push(find([this.pos[0], this.pos[1] + 1], nodes));
+		} else if(!part2 && grid[this.pos[0]][this.pos[1]] === "v") {
+			this.exits.push(find([this.pos[0] + 1, this.pos[1]], nodes));
+		} else {
+			let upNeigh = find([this.pos[0] - 1, this.pos[1]], nodes);
+			let leftNeigh = find([this.pos[0], this.pos[1] - 1], nodes);
+			let downNeigh = find([this.pos[0] + 1, this.pos[1]], nodes);
+			let rightNeigh = find([this.pos[0], this.pos[1] + 1], nodes);
 
 			if(upNeigh !== undefined && upNeigh.type !== "#") this.exits.push(upNeigh);
 			if(leftNeigh !== undefined && leftNeigh.type !== "#") this.exits.push(leftNeigh);
@@ -35,12 +35,17 @@ function day23(input) {
 			}
 			if(slopeCount > 1) {
 				this.type = "X";
-				targetNodes.push(this);
+				if(!part2) {
+					targetNodes.push(this);
+				} else {
+					targetNodes2.push(this);
+				}
 			}
-		//}
+		}
 	}
 
-	function find(pos) {
+
+	function find(pos, nodes) {
 		if(nodes[pos[0]] === undefined) return undefined;
 		return nodes[pos[0]][pos[1]];
 	}
@@ -48,6 +53,7 @@ function day23(input) {
 	const FILE_REGEX = /.+/g;
 	let grid = [];
 	let nodes = [];
+	let nodes2 = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
 		grid.push(entry[0].split(""));
@@ -56,24 +62,36 @@ function day23(input) {
 
 	for(let i = 0; i < grid.length; i++) {
 		let nodeRow = [];
+		let node2Row = [];
 		for(let j = 0; j < grid[i].length; j++) {
 			nodeRow.push(new Node([i, j], grid[i][j]));
+			node2Row.push(new Node([i, j], grid[i][j]));
 		}
 		nodes.push(nodeRow);
+		nodes2.push(node2Row);
 	}
 
 	let targetNodes = [];
+	let targetNodes2 = [];
 
 	for(let row of nodes) {
 		for(let node of row) {
-			node.initialize();
+			node.initialize(nodes, false);
 		}
 	}
 	targetNodes.push(nodes[0][1]);
 	targetNodes.push(nodes[nodes.length - 1][nodes[nodes.length - 1].length - 2]);
 
+	for(let row of nodes2) {
+		for(let node of row) {
+			node.initialize(nodes2, true);
+		}
+	}
+	targetNodes2.push(nodes2[0][1]);
+	targetNodes2.push(nodes2[nodes2.length - 1][nodes2[nodes2.length - 1].length - 2]);
+
 	// Time to plagiarize from my 2023 day 21 (plagiarization)^2 of my 2023 day 10 plagiarization of my 2022 day 18 solution.
-	function BFS(...start) {
+	function BFS(nodes, ...start) {
 		let queue = start;
 		for(let row of nodes) {
 			for(let node of row) {
@@ -98,15 +116,23 @@ function day23(input) {
 	}
 
 	for(let node of targetNodes) {
-		BFS(node);
+		BFS(nodes, node);
 		for(let neighNode of targetNodes) {
 			if(neighNode !== node && neighNode.depth < 1000000) {
 				node.targetDepths.push([neighNode, neighNode.depth]);
 			}
 		}
 	}
+	for(let node of targetNodes2) {
+		BFS(nodes2, node);
+		for(let neighNode of targetNodes2) {
+			if(neighNode !== node && neighNode.depth < 1000000) {
+				node.targetDepths.push([neighNode, neighNode.depth]);
+			}
+		}
+	}
 
-	function hike2(visited, length = 0) {
+	function hike2(targetNodes, visited, length = 0) {
 		let currNode = visited[visited.length - 1];
 		if(currNode === targetNodes[targetNodes.length - 1]) {
 			return length;
@@ -114,7 +140,7 @@ function day23(input) {
 		let lengths = [];
 		for(let neighbor of currNode.targetDepths) {
 			if(!visited.includes(neighbor[0])) {
-				lengths.push(hike2([...visited, neighbor[0]], length + neighbor[1] ));
+				lengths.push(hike2(targetNodes, [...visited, neighbor[0]], length + neighbor[1] ));
 			}
 		}
 		if(lengths.length === 0) {
@@ -123,23 +149,8 @@ function day23(input) {
 		return Math.max(...lengths);
 	}
 
-	function hike(visited) {
-		let currNode = visited[visited.length - 1];
-		if(currNode.pos[0] === nodes.length - 1) {
-			return visited.length;
-		}
-		let lengths = [];
-		for(let neighbor of currNode.exits) {
-			if(!visited.includes(neighbor)) {
-				lengths.push(hike([...visited, neighbor]));
-			}
-		}
-		if(lengths.length === 0) {
-			return 0;
-		}
-		return Math.max(...lengths);
-	}
-
-	let trail = hike2([nodes[0][1]]);
+	let trail = hike2(targetNodes, [nodes[0][1]]);
+	let trail2 = hike2(targetNodes2, [nodes2[0][1]]);
 	displayCaption(`The max length is ${trail}.`);
+	displayCaption(`The surprisingly dry max length is ${trail2}.`);
 }
