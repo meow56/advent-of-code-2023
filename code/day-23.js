2daccce40246047c13bda10a5b191716f66e5f1f
meow56 <spinattacktotheface@gmail.com>
2023-12-23 00:40:46 -0600

    Day 23-4
    Ok, visualization wasn't too bad.

--- code/day-23.js
+++ code/day-23.js
@@ -8,6 +8,7 @@ function day23(input) {
 
 		this.depth = Infinity;
 		this.explored = false;
+		this.predecessor;
 
 		this.targetDepths = [];
 	}
@@ -109,6 +110,7 @@ function day23(input) {
 				if(!path.explored) {
 					path.explored = true;
 					path.depth = next.depth + 1;
+					path.predecessor = next;
 					if(path.type !== "X") queue.push(path);
 				}
 			}
@@ -119,7 +121,11 @@ function day23(input) {
 		BFS(nodes, node);
 		for(let neighNode of targetNodes) {
 			if(neighNode !== node && neighNode.depth < 1000000) {
-				node.targetDepths.push([neighNode, neighNode.depth]);
+				let path = [neighNode];
+				while(path[path.length - 1] !== node) {
+					path.push(path[path.length - 1].predecessor);
+				}
+				node.targetDepths.push([neighNode, neighNode.depth, path]);
 			}
 		}
 	}
@@ -127,7 +133,11 @@ function day23(input) {
 		BFS(nodes2, node);
 		for(let neighNode of targetNodes2) {
 			if(neighNode !== node && neighNode.depth < 1000000) {
-				node.targetDepths.push([neighNode, neighNode.depth]);
+				let path = [neighNode];
+				while(path[path.length - 1] !== node) {
+					path.push(path[path.length - 1].predecessor);
+				}
+				node.targetDepths.push([neighNode, neighNode.depth, path]);
 			}
 		}
 	}
@@ -135,22 +145,77 @@ function day23(input) {
 	function hike2(targetNodes, visited, length = 0) {
 		let currNode = visited[visited.length - 1];
 		if(currNode === targetNodes[targetNodes.length - 1]) {
-			return length;
+			return [length, visited];
 		}
 		let lengths = [];
 		for(let neighbor of currNode.targetDepths) {
 			if(!visited.includes(neighbor[0])) {
-				lengths.push(hike2(targetNodes, [...visited, neighbor[0]], length + neighbor[1] ));
+				lengths.push(hike2(targetNodes, [...visited, neighbor[0]], length + neighbor[1]));
 			}
 		}
 		if(lengths.length === 0) {
-			return 0;
+			return [0, []];
 		}
-		return Math.max(...lengths);
+		let currMax = [0, []];
+		for(let len of lengths) {
+			if(len[0] > currMax[0]) {
+				currMax = len;
+			}
+		}
+		return currMax;
 	}
 
 	let trail = hike2(targetNodes, [nodes[0][1]]);
 	let trail2 = hike2(targetNodes2, [nodes2[0][1]]);
-	displayCaption(`The max length is ${trail}.`);
-	displayCaption(`The surprisingly dry max length is ${trail2}.`);
+	displayCaption(`The max length is ${trail[0]}.`);
+	displayCaption(`The surprisingly dry max length is ${trail2[0]}.`);
+	displayCaption(`The longest trail is shown.`);
+	displayCaption(`The walls are shown as █, while the trail taken is shown as ▒.`);
+	displayCaption(`Use the button to switch between parts.`);
+
+	function display(nodes, trail) {
+		let visited = [];
+		for(let i = 0; i < trail[1].length - 1; i++) {
+			for(let node of trail[1][i].targetDepths) {
+				if(node[0] === trail[1][i + 1]) {
+					visited.push(...node[2]);
+				}
+			}
+		}
+
+		for(let i = 0; i < nodes.length; i++) {
+			let displayLine = "";
+			for(let j = 0; j < nodes.length; j++) {
+				if(visited.includes(nodes[i][j])) {
+					displayLine += "▒";
+				} else if(nodes[i][j].type === "X" || nodes[i][j].type === ".") {
+					displayLine += " ";
+				} else if(nodes[i][j].type === "#") {
+					displayLine += "█";
+				} else {
+					displayLine += nodes[i][j].type;
+				}
+			}
+			displayText(displayLine);
+		}
+	}
+
+	function closure() {
+		let part2 = false;
+		display(nodes, trail);
+
+		function switchPart() {
+			part2 = !part2;
+			clearText();
+			if(!part2) {
+				display(nodes, trail);
+			} else {
+				display(nodes2, trail2);
+			}
+		}
+
+		return switchPart;
+	}
+
+	assignButton(closure(), "Switch part");
 }
